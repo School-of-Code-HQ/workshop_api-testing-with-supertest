40767a665122441e3d9d88858f2c197778dd38ab
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2021-12-20 15:35:38 +0000

    Updated task-2 to ESM, few other tweaks

--- task-2/models/users.js
+++ task-2/models/users.js
@@ -1,29 +1,26 @@
 /**
- * For simplicity and just while we're starting to test our APIs, we're back to using an in-memory array (rather than an actual database).
+ * For simplicity we're temporarily back to using an in-memory array (rather than an actual database).
  */
 const users = [
-  { id: 0, username: "A" },
-  { id: 1, username: "B" },
-  { id: 2, username: "C" },
-  { id: 3, username: "D" },
-  { id: 4, username: "E" },
-  { id: 5, username: "F" },
-  { id: 6, username: "G" },
-  { id: 7, username: "H" },
-  { id: 8, username: "I" },
-  { id: 9, username: "J" },
-  { id: 10, username: "K" },
+  { id: 0, username: "Agatha" },
+  { id: 1, username: "Barbara" },
+  { id: 2, username: "Chase" },
+  { id: 3, username: "David" },
+  { id: 4, username: "Elvis" },
+  { id: 5, username: "Fiona" },
+  { id: 6, username: "Greg" },
+  { id: 7, username: "Harry" },
+  { id: 8, username: "Ivan" },
+  { id: 9, username: "John" },
+  { id: 10, username: "Kiera" },
 ];
 
-const getUsers = () => {
-  return [...users];
-};
+export function getUsers() {
+  return users;
+}
 
-const getUserById = (idToFind) => {
-  return users.find((user) => user.id === idToFind);
-};
-
-module.exports = {
-  getUsers,
-  getUserById,
-};
+export function getUserById(idToFind) {
+  return users.find(function (user) {
+    return user.id === idToFind;
+  });
+}
